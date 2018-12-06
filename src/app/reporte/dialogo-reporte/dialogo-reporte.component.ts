f480c040ffb0740cdac10bf92117538ce2380b9a
Leandro Devoto <ldevoto@hotmail.es>
2018-12-05 23:39:27 -0300

    Se avanza con el reporte

--- src/app/reporte/dialogo-reporte/dialogo-reporte.component.ts
+++ src/app/reporte/dialogo-reporte/dialogo-reporte.component.ts
@@ -11,6 +11,27 @@ import ParametrosReporte from 'src/app/domain/ParametrosReporte';
 export class DialogoReporteComponent implements OnInit {
   
   cargando: boolean = true;
+  lineas = [
+    {
+      tipo: "Vida",
+      en_curso: {
+        cantidad: 121,
+        valor: 212322.34
+      },
+      suspendida: {
+        cantidad: 32,
+        valor: 34323.54
+      },
+      anulada: {
+        cantidad: 9,
+        valor: 444321.00
+      },
+      finalizada: {
+        cantidad: 940,
+        valor: 93234332.32
+      }
+    }
+  ]
 
   constructor(
     public dialogRef: MatDialogRef<DialogoReporteComponent>,
